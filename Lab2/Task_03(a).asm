f7c6a5fe1fa3ef43c55e9f0d50dd4781171ab335
Atiqa Shaheen <164722171+AtiqaShaeen@users.noreply.github.com>
2024-03-25 21:17:43 +0500

    Update Task_03(a).asm

--- Lab2/Task_03(a).asm
+++ Lab2/Task_03(a).asm
@@ -1,20 +1,13 @@
-TITLE Evaluating the expression,  (ArithmeticExpression1.asm)
-; This program evaluates the expression “5+(6-2)”
-; using one register only
+TITLE Equation Solve,(task_4.asm)
+; This program solve the arithmetic expression “5+(6-2)”
 INCLUDE Irvine32.inc
 .data
-
 .code
 main PROC
-; Storing 0 initially in the Register
-mov eax, 0h
-
-; Evaluating the expression
-mov eax, 6h		 ; Storing value 6 in eax
-sub eax, 2h		 ; Subtracting value 2 from eax
-add eax, 5h		 ; Add value 5 in eax
-
-call DumpRegs 	         ; display the registers
+mov eax,6h ; store 6 in eax register
+sub eax,2h ; subrtact 2 eax (4)
+add eax,5h ; add 5 in the eax (9)
+call DumpRegs ; display the registers 
 exit
 main ENDP
 END main
