a16151d7a9faac19a76cb3bb3fed50a79a6bf738
Atiqa Shaheen <164722171+AtiqaShaeen@users.noreply.github.com>
2024-03-25 21:17:22 +0500

    Update Task_02.asm

--- Lab2/Task_02.asm
+++ Lab2/Task_02.asm
@@ -1,35 +1,22 @@
-TITLE Swapping the Variable,  (Swap.asm)
-; This program swaps 8-bit unsigned integers and 
-; displays the result before and after swapping
+TITLE Swapping Values,(t_3.asm)
+; This program swaps the values of two variables
 INCLUDE Irvine32.inc
 .data
-val1 BYTE 20h		;val1 declared as a variable of type BYTE and initialized
-val2 BYTE 30h
-tempVal BYTE ?         ;tempVal declared as a variable of type BYTE to store values temporarily
+val1 BYTE 20h ;val1 declared as a variable of type BYTE and initialized
+val2 BYTE 30h ;val2 declared as a variable of type BYTE and initialized
 .code
 main PROC
-; Storing 0 initially in Registers
 mov eax, 0h
 mov ebx, 0h
-
-; Storing values in Registers and displaying before Swapping
-mov al,val1		 ; Storing value 1 in eax
-mov bl,val2		 ; Storing value 2 in ebx
-call DumpRegs 	         ; display the registers
-
-; Swapping the Data variable
-mov al, val1
-mov tempVal, al     ; Storing value of val1 in tempVal
-mov al, val2
-mov val1, al        ; Storing value of val2 in val1
-mov al, tempVal
-mov val2, al        ; Storing value oftempVal to val2
-
-; Storing values in Registers and displaying after Swapping
-mov al,val1		 ; Storing value 1 in eax
-mov bl,val2		 ; Storing value 2 in ebx
-call DumpRegs 	         ; display the registers
-
+mov ecx, 0h
+mov al,val1 ; store val1 value in al register
+mov bl,val2 ; store val2 value in bl register
+call DumpRegs ; display the registers before swapping
+mov cl,al ; store al value in cl register
+mov al,bl ; store bl value in al register
+mov bl,cl ; store cl value that is al value in bl register
+mov ecx, 0h
+call DumpRegs ; display the registers after swapping
 exit
 main ENDP
 END main
